67d19f92da2d0e3f8399e7cd79252965a735b949
Halima_Djire <halimadjire76@gmail.com>
2026-01-12 15:44:22 +0100

    Ajout couleurs charte graphique

--- app/src/main/java/com/miage/learnity/ui/theme/Color.kt
+++ app/src/main/java/com/miage/learnity/ui/theme/Color.kt
@@ -2,6 +2,21 @@ package com.miage.learnity.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Couleurs Learnity
+val BackgroundColor = Color(0xFFF5F5F5)
+val PrimaryPurple = Color(0xFF9E5ECE)
+val GradientOrange1 = Color(0xFFFF4B2B)
+val GradientOrange2 = Color(0xFFFF8C37)
+val GradientGreen1 = Color(0xFF1D976C)
+val GradientGreen2 = Color(0xFF93F9B9)
+val GradientBlue1 = Color(0xFF4E54C8)
+val GradientBlue2 = Color(0xFF8F94FB)
+val White = Color(0xFFFFFFFF)
+val TextDark = Color(0xFF1A1A2E)
+val TextGray = Color(0xFF4A4A4A)
+val Success = Color(0xFF4CAF50)
+val CardBackground = Color(0xFFFFFFFF)
+// Couleurs par défaut (pour Theme.kt)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
